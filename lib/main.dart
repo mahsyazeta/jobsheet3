483300d97dad4bdc53690150efecfd16b360990c
mahsyazeta <zetadhana@gmail.com>
2022-03-08 04:49:29 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,83 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-void main() {
+void void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  TextEditingController controllerInput = TextEditingController();
+
+  double _inputUser = 0.0;
+  double _kelvin = 0.0;
+  double _reamur = 0.0;
+
+  void konversi() {
+    setState(() {
+      _inputUser = double.parse(controllerInput.text);
+      _kelvin = _inputUser + 273.15;
+      _reamur = (_inputUser * 4) / 5;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Temperature Converter',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Converter Suhu"),
+          title: const Text('Konverter Suhu'),
         ),
         body: Container(
-          margin: EdgeInsets.all(8),
-          child: TextField(),
+          margin: const EdgeInsets.all(8),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Input(controllerInput: controllerInput),
+              Output(kelvin: _kelvin, reamur: _reamur),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Container(
+                    height: 50,
+                    child: TextButton(
+                      style: TextButton.styleFrom(
+                        backgroundColor: Colors.blue,
+                      ),
+                      onPressed: konversi,
+                      child: Text(
+                        'Konversi Suhu',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
-        ),
+      ),
     );
+  }
+}
+
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
   }
   
 }
